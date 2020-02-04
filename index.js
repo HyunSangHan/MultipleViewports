f5c28068f5972b452b8a3f0ab3045c8ffd4ee81f
HyunSangHan <gustkd3@gmail.com>
2020-02-04 13:33:30 +0900

    Add base structure for extension app

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+document.onreadystatechange = () => {
+  if (document.readyState === 'complete') {
+    // const div = document.createElement('div');
+    // div.innerHTML = `<a href = "https://www.naver.com">Hi Hi Hdi</a>`
+    // document.body.appendChild(div);
+    window.location.href = 'https://www.naver.com';
+  }
+};
