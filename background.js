ba0cd24aa005944a5097d00dec3023ea90b3184e
HyunSangHan <gustkd3@gmail.com>
2020-02-07 20:54:02 +0900

    Sync a tab with sidebar

--- background.js
+++ background.js
@@ -3,13 +3,20 @@
 let prevURL = null;
 
 whale.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  const currentURL = message;
+  const windowType = message.split(" ")[0];
+  const isReLoaded = parseInt(message.split(" ")[1]);
+  const currentURL = message.split(" ")[2];
   console.log(currentURL !== undefined + ", "+ prevURL !== currentURL)
-  if (currentURL !== undefined && prevURL !== currentURL) {
-    sendResponse(`Sync complete!`);
-    // View on sidebar 
-    whale.sidebarAction.show({ url: currentURL, reload: false });
-   // View on active tab
-    // chrome.tabs.update({ url: currentURL, active: true }, tab => {});  
+  if (currentURL !== undefined && prevURL !== currentURL && isReLoaded ===0) {
+    sendResponse(`${isReLoaded + 1}`);
+    if (windowType === "sidebar") {
+      // View on active tab
+        whale.tabs.update({ url: currentURL, active: true }, tab => {});  
+    } else {
+      // View on sidebar 
+      whale.tabs.onUpdated.addListener((tabId, { url }, tab) => {
+        whale.sidebarAction.show({ url: url, reload: false });
+      });
+    }
   };
 });
--- extension_content_script.js
+++ extension_content_script.js
@@ -3,9 +3,17 @@
 window.onload = e => {
   const prevURL = e.srcElement.referrer;
   const currentURL = e.currentTarget.location.href;
+  const splitArray = navigator.userAgent.split(" ");
+  const windowType = splitArray[splitArray.length - 1];
+
+  let isReloaded = 0;
+  console.log(isReloaded)
   prevURL !== currentURL && (
-    whale.runtime.sendMessage(currentURL, response => {
+    whale.runtime.sendMessage(`${windowType} ${isReloaded} ${currentURL}`, response => {
       console.log(response);
+      isReloaded = parseInt(response);
+      console.log("isReloaded before : " + isReloaded)
+      console.log("isReloaded after  : " + parseInt(response))
     })
   )
 }
