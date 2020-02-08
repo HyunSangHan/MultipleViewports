62917d7895d04a884fc38951bc5cb9ea0fe303b4
HyunSangHan <gustkd3@gmail.com>
2020-02-08 15:53:52 +0900

    Add break code for desktop version

--- background.js
+++ background.js
@@ -2,8 +2,10 @@
 
 whale.runtime.onMessage.addListener((message, sender, sendResponse) => {
   const [windowType, currentURL] = message.split(" ");
+  const isTriggeredByTab = currentURL.split("#is_triggered_by_tab#")[1] === ""
+
   if (currentURL !== undefined) {
-    if (windowType === "sidebar") {
+    if (!isTriggeredByTab && windowType === "sidebar") {
       // View on active tab
       sendResponse("Sync complete from the sidebar to the active tab");
       whale.tabs.update({ url: currentURL, active: true }, tab => {});  
@@ -11,7 +13,7 @@ whale.runtime.onMessage.addListener((message, sender, sendResponse) => {
       // View on sidebar 
       sendResponse("Sync complete from the active tab to the sidebar");
       whale.tabs.onUpdated.addListener((tabId, { url }, tab) => {
-        whale.sidebarAction.show({ url: url, reload: false });
+        whale.sidebarAction.show({ url: url + "#is_triggered_by_tab#", reload: false });
       });
     };
   };
