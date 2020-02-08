a11cafde953ea52d8a2c497a18f30ccb16ced0d3
HyunSangHan <gustkd3@gmail.com>
2020-02-08 09:40:33 +0900

    Shorten codes syntax of background.js

--- background.js
+++ background.js
@@ -3,15 +3,12 @@
 let prevURL = null;
 
 whale.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  const windowType = message.split(" ")[0];
-  const isReLoaded = parseInt(message.split(" ")[1]);
-  const currentURL = message.split(" ")[2];
-  console.log(currentURL !== undefined + ", "+ prevURL !== currentURL)
-  if (currentURL !== undefined && prevURL !== currentURL && isReLoaded ===0) {
-    sendResponse(`${isReLoaded + 1}`);
+  const [windowType, isReLoaded, currentURL] = message.split(" ");
+  if (currentURL !== undefined && prevURL !== currentURL && parseInt(isReLoaded) === 0) {
+    sendResponse(`${parseInt(isReLoaded) + 1}`);
     if (windowType === "sidebar") {
       // View on active tab
-        whale.tabs.update({ url: currentURL, active: true }, tab => {});  
+      whale.tabs.update({ url: currentURL, active: true }, tab => {});  
     } else {
       // View on sidebar 
       whale.tabs.onUpdated.addListener((tabId, { url }, tab) => {
