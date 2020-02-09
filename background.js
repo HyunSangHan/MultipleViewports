02033d1bbbbe855f84fbdecedb547aa0c31cba90
HyunSangHan <gustkd3@gmail.com>
2020-02-09 15:56:09 +0900

    Add function - customizeURL

--- background.js
+++ background.js
@@ -1,20 +1,49 @@
 'use strict'
 
+let prevDesktopURL = null;
+
 whale.runtime.onMessage.addListener((message, sender, sendResponse) => {
   const [windowType, currentURL] = message.split(" ");
-  const isTriggeredByTab = currentURL.split("#is_triggered_by_tab#")[1] === ""
-
-  if (currentURL !== undefined) {
-    if (!isTriggeredByTab && windowType === "sidebar") {
+  const targetURL = currentURL.split("#is_triggered_by_tab#")[0];
+  const isTriggeredByTab = currentURL.split("#is_triggered_by_tab#")[1] === "";
+  if (targetURL !== undefined) {
+    if (windowType === "sidebar" && !isTriggeredByTab && prevDesktopURL !== customizeURL(targetURL, windowType)) {
       // View on active tab
       sendResponse("Sync complete from the sidebar to the active tab");
-      whale.tabs.update({ url: currentURL, active: true }, tab => {});  
-    } else {
+      prevDesktopURL = targetURL;
+      whale.tabs.update({ url: customizeURL(targetURL, windowType), active: true }, tab => {});
+      return
+    } else if (windowType !== "sidebar"){
       // View on sidebar 
       sendResponse("Sync complete from the active tab to the sidebar");
-      whale.tabs.onUpdated.addListener((tabId, { url }, tab) => {
-        whale.sidebarAction.show({ url: url + "#is_triggered_by_tab#", reload: false });
-      });
+      prevDesktopURL = targetURL;
+      whale.sidebarAction.show({ url: customizeURL(targetURL, windowType) + "#is_triggered_by_tab#", reload: false });
+      return
     };
   };
 });
+
+whale.tabs.onUpdated.addListener((tabId, { url }, tab) => {
+  prevDesktopURL = url;
+  url !== undefined && whale.sidebarAction.show({ url: customizeURL(url, null) + "#is_triggered_by_tab#", reload: false });
+});
+
+const customizeURL = (url, type) => {
+  const parsedURLInfo = parseURL(url);
+  let replacedURL = url;
+  if (parsedURLInfo) {
+    const [service, from, to] = parsedURLInfo;
+    if (type === "sidebar" && to === "mobile") {
+      replacedURL = url;
+    } else if (type !== "sidebar" && to === "desktop") { 
+      replacedURL = url;
+    } else if (service === "naverblog" && to === "desktop") { 
+      const blogMobileURL = url.replace(serviceDomains[service][from], serviceDomains[service][to]).split("&proxyReferer=")[1];
+      replacedURL = url.replace(serviceDomains[service][from], serviceDomains[service][to]);
+      blogMobileURL && ( replacedURL = decodeURIComponent(blogMobileURL) )
+    } else {
+      replacedURL = url.replace(serviceDomains[service][from], serviceDomains[service][to]);
+    }
+  }
+  return replacedURL;
+}
