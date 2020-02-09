a640f50f51f49c5bdcaf38528202e9fd5b9d946d
HyunSangHan <gustkd3@gmail.com>
2020-02-09 20:23:06 +0900

    Resolve duplicate call about cutomizeURL function

--- background.js
+++ background.js
@@ -6,26 +6,28 @@ whale.runtime.onMessage.addListener((message, sender, sendResponse) => {
   const [windowType, currentURL] = message.split(" ");
   const targetURL = currentURL.split("#is_triggered_by_tab#")[0];
   const isTriggeredByTab = currentURL.split("#is_triggered_by_tab#")[1] === "";
+  const customizedURL = customizeURL(targetURL, windowType);
   if (targetURL !== undefined) {
-    if (windowType === "sidebar" && !isTriggeredByTab && prevDesktopURL !== customizeURL(targetURL, windowType)) {
+    if (windowType === "sidebar" && !isTriggeredByTab && prevDesktopURL !== customizedURL) {
       // View on active tab
       sendResponse("Sync complete from the sidebar to the active tab");
       prevDesktopURL = targetURL;
-      whale.tabs.update({ url: customizeURL(targetURL, windowType), active: true }, tab => {});
+      whale.tabs.update({ url: customizedURL, active: true }, tab => {});
       return
     } else if (windowType !== "sidebar"){
       // View on sidebar 
       sendResponse("Sync complete from the active tab to the sidebar");
       prevDesktopURL = targetURL;
-      whale.sidebarAction.show({ url: customizeURL(targetURL, windowType) + "#is_triggered_by_tab#", reload: false });
+      whale.sidebarAction.show({ url: customizedURL + "#is_triggered_by_tab#", reload: false });
       return
     };
   };
 });
 
 whale.tabs.onUpdated.addListener((tabId, { url }, tab) => {
+  const customizedURL = customizeURL(url, null);
   prevDesktopURL = url;
-  url !== undefined && whale.sidebarAction.show({ url: customizeURL(url, null) + "#is_triggered_by_tab#", reload: false });
+  url !== undefined && whale.sidebarAction.show({ url: customizedURL + "#is_triggered_by_tab#", reload: false });
 });
 
 const serviceDomains = {
